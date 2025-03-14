4a6e5f406faf015d248787430f900d396796659a
Zene <oinkymail3@gmail.com>
2025-03-14 10:50:39 +0000

    Added rotation to Vector2

--- src/vectors.rs
+++ src/vectors.rs
@@ -1,3 +1,6 @@
+use std::ops::Neg;
+
+use zs_core::Radian;
 use zs_macros::generate_vector;
 
 use crate::real;
@@ -17,3 +20,53 @@ pub type Vec4 = Vector4<real>;
 pub type Vec2I = Vector2<i32>;
 pub type Vec3I = Vector3<i32>;
 pub type Vec4I = Vector4<i32>;
+
+impl<S: Neg<Output = S>> Vector2<S>
+{
+    pub fn rotated_90(self) -> Self
+    {
+        return Self
+        {
+            x: -self.y,
+            y: self.x
+        };
+    }
+    pub fn rotated_270(self) -> Self
+    {
+        return Self
+        {
+            x: self.y,
+            y: -self.x
+        };
+    }
+}
+impl<S: zs_core::Float> Vector2<S>
+    where Radian<S>: Into<S>
+{
+    pub fn rotated(self, angle: Radian<S>) -> Self
+    {
+        let a: S = angle.into();
+        let sin = a.sin();
+        let cos = a.cos();
+        
+        return Self
+        {
+            x: (self.x * cos) - (self.y * sin),
+            y: (self.x * sin) + (self.y * cos)
+        };
+    }
+    pub fn rotated_point(self, point: Self, angle: Radian<S>) -> Self
+    {
+        let a: S = angle.into();
+        let sin = a.sin();
+        let cos = a.cos();
+        
+        let vec = self - point;
+        
+        return Self
+        {
+            x: (vec.x * cos) - (vec.y * sin) + point.x,
+            y: (vec.x * sin) + (vec.y * cos) + point.y
+        };
+    }
+}
